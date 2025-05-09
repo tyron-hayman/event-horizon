189274860d3b5ce58d2d97d948c32ac5dd00f8d9
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-05-09 12:19:34 -0700

    migration to supabase for authentication, sign is working

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
 import { motion } from 'motion-v'
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import { supabase } from '@/utilities/supabaseClient'
-import type { Users } from '@/types/types'
+import type { Ref } from 'vue'
 
-const usersdata: Array<Users> = ref([])
-
-async function getUsers() {
-  const { data: users, error } = await supabase.from('users').select('*')
-  usersdata.value = users
-  console.log(usersdata.value)
-}
-onMounted(() => {
-  getUsers()
+const userData: Ref = ref({
+  email: '',
+  password: '',
 })
+
+const signInUser = async (event: MouseEvent) => {
+  event.preventDefault()
+  try {
+    const { data } = await supabase.auth.signInWithPassword({
+      email: userData.value.email,
+      password: userData.value.password,
+    })
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+  return false
+}
 
 const variants = {
   hidden: { opacity: 0, y: 50 },
@@ -37,16 +45,24 @@ const variants = {
       <h1 class="text-white text-5xl pb-5">Client Portal</h1>
       <div class="loginForm w-full">
         <form class="w-full">
-          <input type="text" name="uName" class="formInputs w-full !mb-5" placeholder="Email*" />
+          <input
+            type="text"
+            name="uName"
+            class="formInputs w-full !mb-5"
+            placeholder="Email*"
+            v-model="userData.email"
+          />
           <input
             type="password"
             name="uName"
             class="formInputs w-full !mb-5"
             placeholder="Password*"
+            v-model="userData.password"
           />
           <div class="">
             <button
               class="text-white text-md bg-blue-500 hover:bg-black rounded-3xl px-10 py-2 cursor-pointer !mb-5"
+              v-on:click="signInUser"
             >
               Login
             </button>
