b1eb21aa4086b5e9dcb0ca459eae64829c3bb7aa
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-05-20 00:23:26 -0700

    Build error fixed due to lenis import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.css'
 import './assets/styles/global.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import LenisVue from 'lenis/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(LenisVue)
 
 app.mount('#app')
