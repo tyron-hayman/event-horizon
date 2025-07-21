5177a9b21d1f2996d7df8cb8a2a3fe74ee07144b
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-07-21 13:55:33 -0700

    eslint error fix for node module type errors

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,8 @@
-import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -18,11 +15,10 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
+  {
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**'],
+  },
   vueTsConfigs.recommended,
-
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
@@ -32,5 +28,4 @@ export default defineConfigWithVueTs(
     ...pluginCypress.configs.recommended,
     files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
   },
-  skipFormatting,
 )
