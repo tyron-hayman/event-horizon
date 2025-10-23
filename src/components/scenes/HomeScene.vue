a6826aa4aed32dc6f6d56f466244d40f81d38b87
Tyron Hayman <tyron.hayman@gmail.com>
2025-10-23 15:52:12 -0700

    Updated loading animation and logo

--- src/components/scenes/HomeScene.vue
+++ src/components/scenes/HomeScene.vue
@@ -1,24 +1,34 @@
 <script lang="ts" setup>
 import { TresCanvas } from '@tresjs/core'
-import { Suspense } from 'vue';
+import { Suspense } from 'vue'
 import StatueMain from './StatueMain.vue'
 import { useProgress } from '@tresjs/cientos'
-import { easeOut, motion } from 'motion-v';
+import { anticipate, motion } from 'motion-v'
 
 const { hasFinishLoading, progress, items } = await useProgress()
 </script>
 
 <template>
   <motion.div
-    class="fixed left-0 bottom-0 top-0 z-[5] bg-rose-700 overflow-hidden"
-    :initial="{ width : '100%' }"
-    :animate="{ width : hasFinishLoading ? '0%' : '100%'  }"
-    :transition="{ duration : 0.75, ease: easeOut, delay: 0.75}"
+    class="fixed left-0 bottom-0 top-0 right-0 z-[100] bg-[rgb(5,5,5)] overflow-hidden"
+    :initial="{ bottom: '0%' }"
+    :animate="{ bottom: hasFinishLoading ? '105%' : '0%' }"
+    :transition="{ duration: 0.75, ease: anticipate, delay: 1 }"
   >
-    <div class="absolute left-10 bottom-10 z-[1] mix-blend-difference">
-      <p class="text-white text-[20dvw]">{{ Math.round(progress) }}%</p>
+    <div
+      class="absolute z-[1] w-[150px] h-[150px] left-[50%] top-[50%] -translate-x-[75px] -translate-y-[75px] !bg-cover"
+      :style="{ background: `url(./images/logo.jpg) center center no-repeat` }"
+    ></div>
+    <div
+      class="absolute left-0 bottom-0 w-full h-[30px] z-[2] w-[5px] border-t-white/10 border-t-1 border-solid"
+    >
+      <div
+        class="absolute left-0 top-0 bottom-0 bg-white transition-[width] duration-1000 ease-in-out overflow-hidden flex items-center justify-end"
+        :style="{ width: `${progress}%` }"
+      >
+        <p class="text-black text-xs !font-semibold leading-[1] p-0 !m-0 pr-5">{{ progress }}%</p>
+      </div>
     </div>
-    <div class="absolute left-0 bottom-0 top-0 z-[0] w-[5px] bg-black transition-[width] duration-500 ease-in-out" :style="{ width : `${progress}%`}"></div>
   </motion.div>
   <div class="w-full h-screen fixed z-[0] inset-0">
     <TresCanvas window-size shadows clear-color="#000000" :dpr="1.5" :antialias="false">
